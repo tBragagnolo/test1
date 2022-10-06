77873217a22237b2ec8a85853e181253c1d140bb
tbragagnolo <tbragagnolo@myseneca.ca>
2022-10-06 07:44:58 -0400

    update

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ function onStart(){
 }
 
 app.get("/", (req, res)=>{
-
+    var mes = "";
+    mes += "<h2>Declaration</h2><br>";
+    mes += "<p>I declare that this test is my own work in accordance with Seneca Academic Policy.</p>";
+    mes += "<p>No part of this test has been copied manually or electronically from any other source</p><br>";
+    mes += "<p>Name: Tom Bragagnolo</p><br>";
+    mes += "<p>Student Number: 139157218</p><br>";
+    mes += "<a href = '/test1'>Go to Test1</a>";
+    res.send(mes);
 })
 
 app.get("/test1", (req, res)=>{
