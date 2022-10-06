18a12e19d761fb2472510bf6e232491dd5046df6
tbragagnolo <tbragagnolo@myseneca.ca>
2022-10-06 07:48:37 -0400

    update

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ function onStart(){
 app.get("/", (req, res)=>{
     var mes = "";
     mes += "<h2>Declaration</h2>";
-    mes += "<p>I declare that this test is my own work in accordance with Seneca Academic Policy.</p>";
-    mes += "<p>No part of this test has been copied manually or electronically from any other source</p>";
+    mes += "<p>I declare that this test is my own work in accordance with Seneca Academic Policy.";
+    mes += " No part of this test has been copied manually or electronically from any other source</p>";
     mes += "<p>Name: Tom Bragagnolo</p>";
     mes += "<p>Student Number: 139157218</p>";
     mes += "<a href = '/test1'>Go to Test1</a>";
